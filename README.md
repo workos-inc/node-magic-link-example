bd53f71c778fdcbd8620182548bd3f92e91bc987
Logan Gingerich <logangingerich@yahoo.com>
2020-12-14 14:53:38 -0700

    Adding Node Magic Link Example App

---
+++ README.md
@@ -0,0 +1,35 @@
+# node-magic-link-example
+
+An example application demonstrating how Magic Link works with WorkOS and Node.
+
+## Clone and Install
+
+Clone this repo and install dependencies:
+
+```sh
+git clone git@github.com:workos-inc/node-magic-link-example.git && cd node-magic-link-example && npm install
+```
+
+## Configure your environment
+
+1. Grab your [API Key](https://dashboard.workos.com/api-keys).
+2. Get your [Project ID](https://dashboard.workos.com/sso/configuration).
+3. Create a `.env` file at the root of the project and populate with the
+following environment variables (using values found above):
+
+```typescript
+WORKOS_API_KEY=your_api_key_here
+WORKOS_PROJECT_ID=your_project_id_here
+```
+
+4. Set your [Default Redirect Link](https://dashboard.workos.com/sso/configuration) to `http://localhost:8000/callback`.
+
+## Run the server
+
+```sh
+npm start
+```
+
+Head to `http://localhost:8000/` and enter the email address to which you want to send the Magic Link!
+
+For more information, see the [WorkOS Magic Link documentation](https://workos.com/docs/magic-link/guide/introduction).
---
+++ index.js
@@ -0,0 +1,75 @@
+/**
+ * External Modules
+ */
+const express = require("express");
+const path = require("path");
+const WorkOS = require('@workos-inc/node').default;
+require('dotenv').config()
+process.on('unhandledRejection', (reason, p) => { throw reason });
+const bodyParser = require('body-parser');
+var parser = require('tld-extract');
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = "8000";
+const workos = new WorkOS(process.env.WORKOS_API_KEY);
+const projectID = process.env.WORKOS_PROJECT_ID;
+
+/**
+ *  App Configuration
+ */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * Route Definitions
+ */
+ app.get("/", async (req, res) => {
+  res.render("index", {
+    title: "Home",
+  });
+});
+
+app.post('/passwordless-auth', async (req, res) => {
+  const email = req.body.email
+  const session_options = {
+    email,
+    type: 'MagicLink',
+  };
+  const session = await workos.passwordless.createSession(session_options);
+  await workos.passwordless.sendSession(session.id);
+
+  res.redirect('/confirmation');
+});
+
+app.get("/confirmation", async (req, res) => {
+  res.render("confirmation", {
+    title: "Confirmation",
+  });
+});
+
+app.get("/success", async (req, res) => {
+  res.render("success", {
+    title: "Success",
+  });
+});
+
+app.get('/callback', async (req, res) => {
+  const { code } = req.query;
+  const profile = await workos.sso.getProfile({
+    code,
+    projectID,
+  });
+
+  res.redirect('/success');
+});
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
