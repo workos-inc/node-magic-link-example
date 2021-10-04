c602ab96e55ec1c918a191f438aa8f35335be52b
Adam Wolfman <47123183+awolfden@users.noreply.github.com>
2021-10-04 15:17:42 -0600

    Update UI w/ Templates, View Engine to EJS, File Cleanup, ReadMe Revamp  (#10)
    * updated UI with the templates and refactored routing
    
    * updated UI, file structure, view engine, and readme
    
    Co-authored-by: Adam Wolfman <adamwolfman@Adams-MBP-3.domain>

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-/**
- * External Modules
- */
 const express = require("express");
 const path = require("path");
 const WorkOS = require('@workos-inc/node').default;
+const bodyParser = require('body-parser');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 require('dotenv').config()
 process.on('unhandledRejection', (reason, p) => { throw reason });
-const bodyParser = require('body-parser');
-var parser = require('tld-extract');
 
-/**
- * App Variables
- */
+// App Variables
 const app = express();
 const port = "8000";
 const workos = new WorkOS(process.env.WORKOS_API_KEY);
-const projectID = process.env.WORKOS_PROJECT_ID;
+const clientID = process.env.WORKOS_CLIENT_ID;
 
-/**
- *  App Configuration
- */
+// App Configuration
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.engine('html', require('ejs').renderFile);
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/**
- * Route Definitions
- */
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname)));
+
+
+
+// Route Definitions
+
  app.get("/", async (req, res) => {
-  res.render("index", {
+  res.render("index.ejs", {
     title: "Home",
   });
 });
@@ -38,31 +40,42 @@ app.post('/passwordless-auth', async (req, res) => {
   const email = req.body.email
   const session_options = {
     email,
-    type: 'MagicLink',
+    type: 'MagicLink'
   };
   // API call to generate a new passwordless session.
   const session = await workos.passwordless.createSession(session_options);
+  console.log(session)
 
   // API call to send email, passing in session ID generate above.
   await workos.passwordless.sendSession(session.id);
 
-  res.redirect('/confirmation');
-});
-
-app.get("/confirmation", async (req, res) => {
-  res.render("confirmation", {
-    title: "Confirmation",
-  });
+  res.render("confirmation.ejs", {
+    email: session.email,
+    link: session.link,
+  })
 });
 
 app.get("/success", async (req, res) => {
-  res.render("success", {
-    title: "Success",
+  // Retrieve auth code from query parameter
+  const code = req.query.code;
+  
+  // Exchange auth code for profile
+  const profile = await workos.sso.getProfileAndToken({
+    code,
+    clientID,
+  });
+
+  p_profile = JSON.stringify(profile);
+  
+  res.render("login_successful.ejs", {
+    profile: p_profile,
   });
 });
-/**
- * Server Activation
- */
+
+
+
+// Server Activation
+
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
